b496d2da66c24d244f422d64dc99cbb45c02884f
OutbreakSource <daniel_ends@yahoo.com>
2022-05-04 11:20:03 -0600

    changes

---
+++ FinalProject_DanielMartinez.py
@@ -0,0 +1,114 @@
+"""Daniel Martinez"""
+import os
+import cv2
+import numpy as np
+import tensorflow as tf
+from object_detection.utils import label_map_util
+from object_detection.utils import visualization_utils as viz_utils
+from object_detection.builders import model_builder
+from object_detection.utils import config_util
+from object_detection.protos import pipeline_pb2
+from google.protobuf import text_format
+
+
+# Paths to load pre-trained models, annotations for hands, and checkpoints from training
+CUSTOM_MODEL_NAME = 'my_ssd_mobnet'
+PRETRAINED_MODEL_NAME = 'ssd_mobilenet_v2_fpnlite_320x320_coco17_tpu-8'  # The pre-trained model from TensorFlow Garden
+LABEL_MAP_NAME = 'label_map.pbtxt'
+paths = {
+    'WORKSPACE_PATH': os.path.join('Tensorflow', 'workspace'),
+    'APIMODEL_PATH': os.path.join('Tensorflow', 'models'),
+    'ANNOTATION_PATH': os.path.join('Tensorflow', 'workspace', 'annotations'),
+    'MODEL_PATH': os.path.join('Tensorflow', 'workspace', 'models'),
+    'CHECKPOINT_PATH': os.path.join('Tensorflow', 'workspace', 'models', CUSTOM_MODEL_NAME),
+}
+
+# Loading modified pipeline, created to classify which letters to be shown
+files = {
+    'PIPELINE_CONFIG': os.path.join('Tensorflow', 'workspace', 'models', CUSTOM_MODEL_NAME, 'pipeline.config'),
+    'LABELMAP': os.path.join(paths['ANNOTATION_PATH'], LABEL_MAP_NAME)
+}
+
+# labels for the letters, assisting the pipeline from the lines above.
+labels = [{'name': 'A', 'id': 1}, {'name': 'D', 'id': 2}, {'name': 'E', 'id': 3}, {'name': 'I', 'id': 4},
+          {'name': 'L', 'id': 5},
+          {'name': 'N', 'id': 6}]
+
+# Loading pipeline into config
+config = config_util.get_configs_from_pipeline_file(files['PIPELINE_CONFIG'])
+
+# Following pipeline protocol to load data for labeling, checkpoints, and models
+pipeline_config = pipeline_pb2.TrainEvalPipelineConfig()
+with tf.io.gfile.GFile(files['PIPELINE_CONFIG']) as f:
+    proto_str = f.read()
+    text_format.Merge(proto_str, pipeline_config)
+pipeline_config.model.ssd.num_classes = len(labels)
+pipeline_config.train_config.batch_size = 4
+
+pipeline_config.train_config.fine_tune_checkpoint_type = "detection"
+pipeline_config.train_input_reader.label_map_path = files['LABELMAP']
+pipeline_config.train_input_reader.tf_record_input_reader.input_path[:] = [
+    os.path.join(paths['ANNOTATION_PATH'], 'train.record')]
+pipeline_config.eval_input_reader[0].label_map_path = files['LABELMAP']
+pipeline_config.eval_input_reader[0].tf_record_input_reader.input_path[:] = [
+    os.path.join(paths['ANNOTATION_PATH'], 'test.record')]
+config_text = text_format.MessageToString(pipeline_config)
+with tf.io.gfile.GFile(files['PIPELINE_CONFIG'], "wb") as f:
+    f.write(config_text)
+
+# Load pipeline config and build a detection model
+configs = config_util.get_configs_from_pipeline_file(files['PIPELINE_CONFIG'])
+detection_model = model_builder.build(model_config=configs['model'], is_training=False)
+
+# Load checkpoint from training
+ckpt = tf.compat.v2.train.Checkpoint(model=detection_model)
+ckpt.restore(os.path.join(paths['CHECKPOINT_PATH'], 'ckpt-25')).expect_partial()
+
+
+# TensorFlow function to use Object-detection api to start detecting using frames (images)
+@tf.function
+def detect_fn(image):
+    image, shapes = detection_model.preprocess(image)
+    prediction_dict = detection_model.predict(image, shapes)
+    detections = detection_model.postprocess(prediction_dict, shapes)
+    return detections
+
+
+# Creating variable to read labelmap and returns category index (Score during live tracking)
+category_index = label_map_util.create_category_index_from_labelmap(files['LABELMAP'])
+
+# Finally opening camera and grabbing info
+cap = cv2.VideoCapture(0)
+width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+while cap.isOpened():
+    ret, frame = cap.read()
+    image_np = np.array(frame)  # Convert camera frame into an numpy array for TensorFlow calculations
+
+    input_tensor = tf.convert_to_tensor(np.expand_dims(image_np, 0), dtype=tf.float32)  # using NPArray to calculate
+    detections = detect_fn(input_tensor)  # Use method from earlier to detect
+
+    num_detections = int(detections.pop('num_detections'))
+    detections = {key: value[0, :num_detections].numpy()
+                  for key, value in detections.items()}
+    detections['num_detections'] = num_detections
+
+    detections['detection_classes'] = detections['detection_classes'].astype(np.int64)
+
+    label_id_offset = 1
+    image_np_with_detections = image_np.copy()
+
+    # Object detection functions to start creating boxes and classification of what letter is shown
+    viz_utils.visualize_boxes_and_labels_on_image_array(image_np_with_detections, detections['detection_boxes'],
+                                                        detections['detection_classes'] + label_id_offset,
+                                                        detections['detection_scores'], category_index,
+                                                        use_normalized_coordinates=True, max_boxes_to_draw=5,
+                                                        min_score_thresh=.8)
+
+    cv2.imshow('object detection', cv2.resize(image_np_with_detections, (800, 600)))
+
+    if cv2.waitKey(10) & 0xFF == ord('q'):
+        cap.release()
+        cv2.destroyAllWindows()
+        break
